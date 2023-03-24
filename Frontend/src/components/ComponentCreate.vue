cc9cb2bf2b38c97481bb7ee3c77a34f543e00c65
unknown <marcko236@gmail.com>
2023-03-24 12:02:44 -0500

    Se añade router, se crean los modulos listar y crear y se configuran las rutas

---
+++ Frontend/src/components/ComponentCreate.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+
+        <h1>crear empleado</h1>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Frontend/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+import ComponentCreate from '../components/ComponentCreate.vue'
+import ComponentList from '../components/ComponentList.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: ComponentList
+  },
+  
+  {
+    path: '/create',
+    name: 'Creaate',
+    component: ComponentCreate
+  },
+  
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
